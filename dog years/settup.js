5065d64946fa2b20a72d0b4147131c7985e4b4d4
pavich5 <pavic.antonio969@gmail.com>
2022-12-10 19:49:07 +0100

    Homeworks

---
+++ dog years/settup.js
@@ -0,0 +1,26 @@
+let firstNumber = Number(prompt("Vnesi gi kuceskite godini"));
+
+function calculate(firstNumber, dogYear) {
+  if (
+    Number.isNaN(firstNumber) ||
+    Number.isNaN(dogYear) ||
+    typeof x !== "number" ||
+    typeof y !== "number"
+  ) {
+    return "Invalid input";
+  }
+  let result = firstNumber * dogYear;
+  let result2 = humanYear / dogYear;
+  return
+}
+let humanYear = Number(firstNumber);
+let dogYear = 7;
+
+let result = dogYear * humanYear;
+let result2 = humanYear / dogYear;
+console.log(`${dogYear} Dog years in human years is ${result}`);
+console.log(`${humanYear} Human years in dog years is ${result2}`);
+
+
+
+
---
+++ type of/setup.js
@@ -0,0 +1,6 @@
+let string = "String";
+let number = 3.14;
+let code = false;
+let kire;
+let object = {name:'John', age:34};
+console.log(typeof string, typeof number,typeof code,typeof undefined,typeof object)
